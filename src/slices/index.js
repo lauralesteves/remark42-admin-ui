0014d12a2d246807fadbe44a87d59e075e3f4792
Laura Esteves <git@lauraesteves.com>
2026-03-23 18:52:45 -0300

    feat(users): add users Redux slice with blocked list and unblock
    Async thunks for fetching blocked users list and unblocking a user.
    Removes user from state on successful unblock.

--- src/slices/index.js
+++ src/slices/index.js
@@ -4,12 +4,14 @@ import layoutReducer from "./layouts/reducer";
 import authReducer from "./auth/reducer";
 import postsReducer from "./posts/reducer";
 import commentsReducer from "./comments/reducer";
+import usersReducer from "./users/reducer";
 
 const rootReducer = combineReducers({
   Layout: layoutReducer,
   Auth: authReducer,
   Posts: postsReducer,
   Comments: commentsReducer,
+  Users: usersReducer,
 });
 
 export default rootReducer;
