849fb9c7b073d641a4988a2a2e3c71fc7f9454c2
Laura Esteves <git@lauraesteves.com>
2026-03-23 13:04:04 -0300

    feat(comments): add comments Redux slice with delete and pin
    Async thunks for fetching comments by post, soft-deleting, and
    toggling pin status. Supports sort parameter and clearing state
    on unmount.

--- src/slices/index.js
+++ src/slices/index.js
@@ -3,11 +3,13 @@ import { combineReducers } from "redux";
 import layoutReducer from "./layouts/reducer";
 import authReducer from "./auth/reducer";
 import postsReducer from "./posts/reducer";
+import commentsReducer from "./comments/reducer";
 
 const rootReducer = combineReducers({
   Layout: layoutReducer,
   Auth: authReducer,
   Posts: postsReducer,
+  Comments: commentsReducer,
 });
 
 export default rootReducer;
